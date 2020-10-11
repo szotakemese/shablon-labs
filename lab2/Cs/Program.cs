62b731cffe7c6d1c5315d55eaa23fb1a762fa0ea
Emeshe Sotak <48476041+szotakemese@users.noreply.github.com>
2020-10-11 19:55:56 +0200

    Add files via upload

---
+++ lab2/Cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton single = Singleton.GetInstance();
+            single.AddLink("https://classroom.google.com/");
+            single.AddLink("https://www.uzhnu.edu.ua/");
+            single.AddLink("https://e-learn.uzhnu.edu.ua/");
+            single.AddLink("https://www.uzhnu.edu.ua/");
+            single.AddLink("https://classroom.google.com/");
+            single.AddLink("https://e-learn.uzhnu.edu.ua/");
+            single.AddLink("https://classroom.google.com/");
+            single.AddLink("https://www.uzhnu.edu.ua/");
+            single.AddLink("https://classroom.google.com/");
+            single.PrintAllLinks();
+
+            single.ClearAllLinks();
+            
+            single.AddLink("https://www.uzhnu.edu.ua/");
+            single.AddLink("https://www.uzhnu.edu.ua/");
+            single.PrintAllLinks();
+        }
+    }
+}
